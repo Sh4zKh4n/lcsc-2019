885b0c0efbcd0175c51a2a0f3a2db1cf65753b44
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2019-02-04 14:24:36 +0100

    Add 'Hello, world!' and RNG

---
+++ code/week1/rng.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int rng(unsigned int previous) {
+  return (100 * previous + 5) % 23;
+}
+
+int main() {
+  unsigned int x = 5;
+  for (int i = 0; i < 10; ++i) {
+    auto x = rng(x);
+    std::cout << x << "\n";
+  }
+  return 0;
+}
